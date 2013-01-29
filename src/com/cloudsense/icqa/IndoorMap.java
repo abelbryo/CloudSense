967a043628a220d894dc199495c3fceecd68d66d
Abel Terefe <abelbryo@gmail.com>
2013-01-29 16:38:39 +0200

    Added a warped ground overlay

--- src/com/cloudsense/icqa/IndoorMap.java
+++ src/com/cloudsense/icqa/IndoorMap.java
@@ -1,16 +1,22 @@
 package com.cloudsense.icqa;
 
+import android.os.Bundle;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.os.Bundle;
 
 public class IndoorMap extends android.support.v4.app.FragmentActivity {
 
 	private GoogleMap mMap;
+	//private GroundOverlay mGroundOverlay;
+	//private BitmapDescriptor image;
+	
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,7 +45,20 @@ public class IndoorMap extends android.support.v4.app.FragmentActivity {
 				.title("TUAS").snippet("Here is where we work");
 		mMap.addMarker(marker);
 		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 17.0f));
+		
+		mMap.addGroundOverlay(new GroundOverlayOptions()
+			//.bearing(12.70f)
+			.image(BitmapDescriptorFactory.fromResource(R.drawable.tuas_warped))
+			.positionFromBounds(setupBounds())
+			.transparency(0.5f));
+		
 
+	}
+	
+	public LatLngBounds setupBounds(){
+		LatLng  northEast = new LatLng(60.18771980524313, 24.820497035972608);
+		LatLng  southWest = new LatLng(60.18677168239048, 24.817380309097302);
+		return new LatLngBounds(southWest, northEast);
 	}
 
 }
